5921eff342ab6b93cce88501bfc016cc4c092978
Artem138 <t.k.200516@gmail.com>
2024-06-13 01:06:27 +0300

    Create question 1.cpp

---
+++ question 1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+// a) Функция для нахождения максимального из двух чисел double
+double max(double num1, double num2); 
+
+// b) Функция для обмена двух целых чисел
+void swap(int& num1, int& num2);
+
+// c) Функция для нахождения наибольшего элемента в динамическом массиве
+int& getLargestElement(int* arr, int length); 
